3b81ab8bf18ffb174f3fbed2c06fb05e7888d133
Benjamin Mildren <mildrenben@gmail.com>
2020-03-30 16:56:04 +0100

    Basic docs website setup

---
+++ docs-website/app.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router } from '@reach/router'
+
+const App = () => <h1>HELLO</h1>
+
+ReactDOM.render(<App />, document.getElementById('app'))
