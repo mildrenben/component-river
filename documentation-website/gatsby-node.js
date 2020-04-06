99c42309d578856fee4ebb98adf48fbe842ce0f5
Benjamin Mildren <mildrenben@gmail.com>
2020-04-06 17:19:11 +0100

    Add gh-pages package and deploy docs site to github pages

--- documentation-website/gatsby-node.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const fs = require('fs');
-
-exports.onPostBuild = function () {
-  fs.renameSync(path.join(__dirname, 'public'), path.join(__dirname, '../docs'));
-};
